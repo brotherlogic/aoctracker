e255fddfb17c12331660a0a8c13f32eb865d6731
Simon Tucker <brotherlogic@gmail.com>
2023-03-08 08:17:12 -0800

    First pass at tracker

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+
+	pb "github.com/brotherlogic/aoctracker/proto"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"google.golang.org/grpc"
+)
+
+var (
+	port        = flag.Int("port", 8080, "The server port.")
+	metricsPort = flag.Int("metrics_port", 8081, "Metrics port")
+)
+
+var (
+	year = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "aoctracker_year",
+		Help: "The year",
+	})
+)
+
+type Server struct {
+}
+
+func (s *Server) Track(_ context.Context, req *pb.TrackRequest) (*pb.TrackResponse, error) {
+	year.Set(float64(req.GetCurrentYear()))
+	return &pb.TrackResponse{}, nil
+}
+
+func main() {
+	flag.Parse()
+
+	s := &Server{}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("aoctracker failed to listen on the serving port %v: %v", *port, err)
+	}
+	gs := grpc.NewServer()
+	pb.RegisterAOCTrackerServiceServer(gs, s)
+
+	// Setup prometheus export
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		http.ListenAndServe(fmt.Sprintf(":%v", *metricsPort), nil)
+	}()
+
+	if err := gs.Serve(lis); err != nil {
+		log.Fatalf("aoctracker failed to serve: %v", err)
+	}
+}
